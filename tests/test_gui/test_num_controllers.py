5255625a5b2bdc53877aa29ea51b209e6d65143f
mariuzka <mariuzka@users.noreply.github.com>
2026-02-09 21:20:55 +0000

    auto-commit: fixes by ruff

--- tests/test_gui/test_num_controllers.py
+++ tests/test_gui/test_num_controllers.py
@@ -2,6 +2,7 @@ from mesa.examples.basic.boid_flockers.model import BoidFlockers
 import mesarcade as mesar
 from dataclasses import dataclass
 
+
 @dataclass
 class Event:
     new_value: float
@@ -9,8 +10,13 @@ class Event:
 
 def create_canvas():
     population_size_controller = mesar.NumController(
-        "population_size", 100, 25, 1000, 25, "n birds",
-        )
+        "population_size",
+        100,
+        25,
+        1000,
+        25,
+        "n birds",
+    )
     canvas = mesar.Canvas(
         model_class=BoidFlockers,
         controllers=[population_size_controller],
@@ -19,11 +25,12 @@ def create_canvas():
     canvas._setup()
     return canvas
 
+
 def test_num_controller_buttons():
     canvas = create_canvas()
-    
+
     controller = canvas.renderer.controllers[0]
-    
+
     # test basic setup
     assert controller.parameter_name == "population_size"
     assert controller.label == "n birds"
@@ -42,23 +49,23 @@ def test_num_controller_buttons():
     # test maximum
     for _ in range(500):
         controller.buttons.increase_button.on_click(None)
-    
+
     assert controller.parameter_value == 1000
     assert canvas.renderer.parameter_dict["population_size"] == 1000
 
     # test minimum
     for _ in range(500):
         controller.buttons.decrease_button.on_click(None)
-    
+
     assert controller.parameter_value == 25
     assert canvas.renderer.parameter_dict["population_size"] == 25
-    
+
     canvas.window.close()
 
 
 def test_num_controller_slider():
     canvas = create_canvas()
-    
+
     controller = canvas.renderer.controllers[0]
 
     assert controller.parameter_value == 100
@@ -72,7 +79,7 @@ def test_num_controller_slider():
 
 def test_increase_and_reset():
     canvas = create_canvas()
-    
+
     controller = canvas.renderer.controllers[0]
 
     assert len(canvas.renderer.model.agents) == 100
